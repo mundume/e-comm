9d5e03a05e72d61d570a1efcbff32101b9003e8d
Nzai <nzai@localhost.localdomain>
2022-04-25 19:23:18 +0300

    hero component

---
+++ src/data.js
@@ -0,0 +1,25 @@
+
+export const sliderItems = [
+
+    {
+      id: 1,
+      img: "https://images.unsplash.com/photo-1650784424602-b892f7292b9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      title: "NEW ARRIVALS",
+      desc: "Check out our new arrivals!",
+      bg: "lightblue",
+    },
+    {
+      id: 2,
+      img: "https://images.unsplash.com/photo-1650784424602-b892f7292b9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "lightblue",
+    },
+    {
+      id: 3,
+      img: "https://images.unsplash.com/photo-1519238263530-99bdd11df2ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8a2lkcyUyMHdlYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60",
+      title: "KIDS WEAR",
+      desc: "DON'T CO ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "lightblue",
+    },
+  ];
