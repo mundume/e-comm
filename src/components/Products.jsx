8a4fbc2841041cabdfc1d63a5cb3fa122f2f83e1
Nzai <nzaih18@gmail.com>
2022-05-18 16:51:57 +0300

    add-details

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 import styled from 'styled-components'
 import { popularProducts} from '../data'
 import Product from './Product'
 import { mobile } from '../responsive';
 import { ArrowRightOutlined } from '@mui/icons-material';
+import axios from 'axios'
 
 const Container = styled.div`
 
@@ -21,13 +22,35 @@ ${mobile({flexWrap:'noWrap', overflowX:'scroll'})}
 
 
 const Products = ({cat, filters, sort}) => {
-  console.log(cat, filters, sort)
+  const [products, setproducts] = useState([])
+  const [filteredProducts, setFilteredProducts] = useState([])
   
+useEffect(()=>{
+  const getProducts = async ()=>{
+    try{
+      const res = await axios.get( cat ? `http://localhost:5000/api/products?category=${cat}`: "http://localhost:5000/api/products")
+     setproducts(res.data)
+    }catch(err){
+
+    }
+  };
+  getProducts()
+},[cat])
+
+useEffect(()=>{
+  cat && 
+  setFilteredProducts(
+  products.filter((item) =>Object.entries(filters).every(([key,value]) =>
+    item[key].includes(value)
+  ))
+  );
+},[products, cat,filters])
+
   return (
     <Container>
 
       
-      {popularProducts.map((item) => (
+      {filteredProducts.map((item) => (
        
         <Product item={item} key={item.id}
         />
