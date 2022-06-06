35e38d2217fe1acb50e43c2600fb0baa406eff88
Nzai <nzaih18@gmail.com>
2022-06-06 17:23:15 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './translate'
 import { render } from "react-dom";
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import {store , persistor}from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react'
 
 
 const rootElement = document.getElementById("root");
@@ -12,9 +13,11 @@ const rootElement = document.getElementById("root");
 render(
   
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <Suspense fallback={<div>Loading...</div>}>
       <App />
    </Suspense>
+   </PersistGate>
    </Provider>,
   
 
